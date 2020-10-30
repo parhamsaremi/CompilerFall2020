441702538bdd18d64c0b51eda892bff6e47b8b96
parhamsaremi <pa.sa.13792000@outlook.com>
2020-10-30 21:11:19 +0330

    fiexd bug in q6.py regex

--- Assignments/HW1/temp-codes/6.py
+++ Assignments/HW1/temp-codes/6.py
@@ -1,11 +1,11 @@
 import re   
 while True:
     s = input()
-    for i in re.finditer("((-|\+)?0(x|X)([0-9a-fA-F]+))|((-|\+)?(0\d*))|((-|\+)?[1-9]\d+)",s):
+    for i in re.finditer("((-|\+)?0(x|X)([0-9a-fA-F]+))|((-|\+)?(0\d*))|((-|\+)?[1-9]\d*)",s):
         a = i.group(0)
         if re.match("^(-|\+)?(0\d*)$",a):
             print(int(a,8), "octate")
         if re.match("^(-|\+)?0(x|X)([0-9a-fA-F]+)$",a):
             print((int(a,0)), "hex")
-        if re.match("^(-|\+)?[1-9]\d+$",a):
+        if re.match("^(-|\+)?[1-9]\d*$",a):
             print((int(a)), "decimal")
