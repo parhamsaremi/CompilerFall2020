281d93f9c893749e40b39e9380014369b15ef893
sarakh7878 <[srkhsrv21@gmail.com]>
2021-02-14 18:11:59 +0330

    Tests

---
+++ Project/NewTests/Operand/notEqual.d
@@ -0,0 +1,6 @@
+int main() {
+  Print(0 != 1);
+  Print(1 != 1);
+  Print(true != true);
+  Print(true != false);
+}
---
+++ Project/NewTests/String/string2.d
@@ -0,0 +1,9 @@
+int main()
+{
+    Print("hello" == "goodbye");
+    string s;
+    s = ReadLine();
+    Print("hello" == "hello");
+    Print("hello" == s);
+    Print("hello" == s);
+}
