8d021a56d56606c6ebcf99e1c1775c2f209a642b
sarakh7878 <[srkhsrv21@gmail.com]>
2021-02-15 21:29:04 +0330

    MIPS Functions

--- Project/Mips/ReadLine.asm
+++ Project/Mips/ReadLine.asm
@@ -1,24 +1,24 @@
 ReadLine:               li $t0 , $zero
                         li $t1 , 10
-loopReadLine:           li $v0 , 12
+loop:                   li $v0 , 12
                         syscall
                         addi $t0 , $t0 , 1
                         subi $sp , $sp , 1
-                        beq $v0 , $t1 , endLoopReadLine
+                        beq $v0 , $t1 , endLoop
                         sb $v0 , 1($sp)
-                        j loopReadLine
-endLoopReadLine:        sb $zero , 1($sp)
+                        j loop
+endLoop:                sb $zero , 1($sp)
                         li $v0 , 9
                         addi $a0 , $t0 , 0
                         syscall
                         move $t1 , $v0
                         subi $t0 , $t0 , 1
-loopMoveString:         add $t2 , $t1 , $t0
+loop2:                  add $t2 , $t1 , $t0
                         lb $t3 , 1($sp)
                         sb $t3 , 0($t2)
-                        beq $t0 , $zero , endLoopMoveString
+                        beq $t0 , $zero , exit
                         addi $sp , $sp , 1
                         subi $t0 , $t0 , 1
-                        j loopMoveString
-endLoopMoveString:      addi $sp , $sp , 1
+                        j loop2
+exit:                   addi $sp , $sp , 1
                         jr $ra
